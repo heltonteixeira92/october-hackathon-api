e998e6371c326b4ecd754cd6c69f04a20de7b5b1
Helton Teixeira <heltonteixeiradesouza@hotmail.com>
2022-10-09 19:58:11 -0300

    db postgresql config

--- core/settings.py
+++ core/settings.py
@@ -9,17 +9,18 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+from functools import partial
 from pathlib import Path
-from decouple import config
+from decouple import config, Csv
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
 
-DEBUG = config('DEBUG')
+DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -65,12 +66,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'core.wsgi.application'
 
+default_db_url = 'sqlite:///' + str(BASE_DIR/'db.sqlite3')
+
+parse_database = partial(dj_database_url.parse, conn_max_age=600)
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+   'default': config('DATABASE_URL', default=default_db_url, cast=parse_database)
     }
-}
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
